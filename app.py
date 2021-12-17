2ac87a6b3c87c187dadf9e5038caf20c65e43868
Liana Dzhanashyia <likadgani@gmail.com>
2021-12-17 23:45:17 +0200

    wayforpayapi3

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, request
 import telegram
 from telebot.credentials import bot_token, bot_user_name,URL
-from wayforpay import *
+from wayforpay import Api
  
 global bot
 global TOKEN
