b85de130f3cf61f5036fe813f523a7a813e53ca0
Liana Dzhanashyia <likadgani@gmail.com>
2022-02-08 00:03:08 +0200

    first db commit3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 import telegram
 from telebot.credentials import bot_token, bot_user_name,URL
 
-from cloudipsp import Api, Checkout
+
 
 from db import ClientDatabase
 
